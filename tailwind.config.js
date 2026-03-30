7ac3fa28b361d74529e2e848f0a8ba936a186d18
Seydina Limamou Laye <seydinalimamoulayeyade@gmail.com>
2026-03-30 12:42:15 +0000

    refactor: refonte complète de la page d'accueil avec une nouvelle structure HTML, ajout de styles Tailwind personnalisés

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   content: ["./*.html", "./html-pages/**/*.html", "./src/**/*.{html,js,css}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#2c3e50",
+        nav: "#34495e",
+        success: "#2ecc71",
+        info: "#3498db",
+        primary: "#57a8f5",
+        danger: "#dc3545",
+        "danger-alt": "#f44336",
+        blue: "#007bff",
+        "green-link": "#00ff0d",
+        "body-bg": "#f4f4f4",
+        "card-bg": "#f9f9f9",
+      },
+      fontFamily: {
+        sans: ["Arial", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
